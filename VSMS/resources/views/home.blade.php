2b8da2f6719b8f2645037767c1f34268a3e78082
Randiv Dulneth <randiv1212@gmail.com>
2025-12-27 18:27:23 +0530

    1 commit after done project

---
+++ VSMS/resources/views/home.blade.php
@@ -0,0 +1,9 @@
+@extends('layouts.app')
+
+@section('title', 'MotorCare|Home')
+
+@section('content')
+<h1>Welcome to MotorCare</h1>
+<p>Reliable Vehicle Service & Maintenance</p>
+<p>Manage your services easily.</p>
+@endsection
